04934409c1875a9f05fc1d06e26de7967703d977
Robert Tulke <rt@debian.sh>
2017-02-14 13:32:34 +0100

    Create deploy-icinga2-node.sh

---
+++ Icinga2/deploy-icinga2-node.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+usage() {
+    echo `basename $0`: ERROR: $* 1>&2
+    echo usage: `basename $0` '[-h]' 1>&2
+    exit 1
+}
+
+help() {
+    echo usage: `basename $0` '[-h] <icinga.node> <icingagroup>' 1>&2
+    exit 1
+}
+
+while :
+do
+    case "$1" in
+    -h|--help) help ;;
+    -*) usage "bad argument $1";;
+    *) break;;
+    esac
+    shift
+done
+
+if [ -z "$2" ]; then
+    usage
+    exit 1
+fi
+ansible-playbook -i inventory install_icinga2_agent.yml -e hostitem=${1} mygroup=${2}
