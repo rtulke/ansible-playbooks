4870c7227577a77721d73278915470ae77c86556
Robert Tulke <rt@debian.sh>
2017-02-14 16:24:52 +0100

    Update deploy-icinga2-node.sh
    now it works

--- Icinga2/deploy-icinga2-node.sh
+++ Icinga2/deploy-icinga2-node.sh
@@ -25,4 +25,4 @@ if [ -z "$2" ]; then
     usage
     exit 1
 fi
-ansible-playbook -i inventory install_icinga2_agent.yml -e hostitem=${1} mygroup=${2}
+ansible-playbook -i inventory icinga2-node-setup.yml -e hostitem=${1} mygroup=${2}
